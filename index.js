c24440b8eb24d1078e6b3c7943701d007365fe3a
AlifSakib <alifsakib@gmail.com>
2022-11-16 00:47:48 +0600

    database connected

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 
 require("dotenv").config();
 require("colors");
-const app = expres();
+
+const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
@@ -15,6 +17,24 @@ app.get("/", (req, res) => {
   });
 });
 
-app.lister(port, () => {
-  console.log(`Server is running on ${port}`.bgBlack);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4mqdriq.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function dbConnect() {
+  try {
+    await client.connect();
+    console.log(" Database Connection Success.... ".bgGreen);
+  } catch (error) {
+    console.log(error.name.bgRed, error.message.bgRed);
+  }
+}
+
+dbConnect();
+
+app.listen(port, () => {
+  console.log(` Server is running on ${port}... `.bgMagenta);
 });
